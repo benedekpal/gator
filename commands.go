b0f6b79c8315f993a592e3d87a2de5575898c12d
Benedek Pal <benedek.pal1024@gmail.com>
2025-08-25 17:09:51 +0200

    users command that lists all the users and indicates which one is currently logged in

--- commands.go
+++ commands.go
@@ -83,6 +83,30 @@ func handlerClearUsers(s *state, cmd command) error {
 	return nil
 }
 
+func handlerGetUsers(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return errors.New("no arg required")
+	}
+
+	users, err := s.db.GetUsers(context.Background())
+
+	if err != nil {
+		return err
+	}
+
+	currentUser := s.config.CurrentUserName
+
+	for _, user := range users {
+		fmt.Printf("* ")
+		fmt.Printf("%s", user.Name)
+		if user.Name == currentUser {
+			fmt.Printf(" (current)")
+		}
+		fmt.Printf("\n")
+	}
+	return nil
+}
+
 func (c *commands) run(s *state, cmd command) error {
 	command, exists := c.handlers[cmd.name]
 	if !exists {
--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerClearUsers)
+	cmds.register("users", handlerGetUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
